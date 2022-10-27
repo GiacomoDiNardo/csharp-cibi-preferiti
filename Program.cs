9b21e10a93351513a2a634fe9f1b809f6669a504
GiacomoDiNardo <vest_95@hotmail.it>
2022-10-27 10:50:38 +0200

    add bonus condition

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-string[] cibiPreferiti = { "Bistecca di maiale", "Tordelli Lucchesi", "Risotto ai frutti di mare", "Spaghetti allo scoglio", "Pizza", "Pasta all' arrabbiata", "Brodo di verdura" };
+string[] cibiPreferiti = { "Bistecca di maiale", "Tordelli Lucchesi", "Risotto ai frutti di mare", "Spaghetti allo scoglio", "Pizza", "Pasta all' arrabbiata"};
 
 Console.WriteLine("Lunghezza classifica cibi: " + cibiPreferiti.Length);
 Console.WriteLine("Classifica cibi: ");
@@ -13,4 +13,11 @@ for (int i = 0; i < cibiPreferiti.Length; i++)
 
 Console.WriteLine("il mio cibo preferito è " + cibiPreferiti[0]);
 Console.WriteLine("il mio cibo preferito ma non troppo è " + cibiPreferiti[cibiPreferiti.Length-1]);
-Console.WriteLine("il mio cibo mediamente preferito è " + cibiPreferiti[cibiPreferiti.Length/2]);
+
+if(cibiPreferiti.Length % 2 == 0)
+{
+    Console.WriteLine("i miei cibi mediamente preferiti sono " + cibiPreferiti[cibiPreferiti.Length / 2 - 1] + " e " + cibiPreferiti[cibiPreferiti.Length / 2]);
+} else
+{
+    Console.WriteLine("il mio cibo mediamente preferito è " + cibiPreferiti[cibiPreferiti.Length / 2]);
+}
